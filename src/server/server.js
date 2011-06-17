053828f85d7a00b3a87659686dc2243173ad4e67
chencheng (yunqian) <sorrycc@gmail.com>
2011-06-17 23:41:20 +0800

    add upload support and test.sh

--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,15 @@ var express = require('express'),
     mkdirP = require('./libs/mkdir_p'),
     app = express.createServer(form({ keepExtensions: true })),
     port = process.env.PORT || 8888,
-    tasks = {};
+    tasks = {
+        // task for test
+        '12345': {
+            taskId: '12345',
+            url: 'http://www.taobao.com/',
+            status: {},
+            path: '../../imgs/2011/06/17/12345/'
+        }
+    };
 
 app.configure(function() {
     app.use(express.methodOverride());
@@ -31,25 +39,25 @@ app.get('/add', function(req, res) {
     try {
         var query = require('querystring').parse(require('url').parse(req.url).query);
         addTask(query['url']);
-        res.send('ok');
+        res.send("ok\n");
     } catch(e) {
-        res.send('error');
+        res.send("error\n");
     }
+    // console.log(tasks);
 });
 
-app.get('/get/:clientId', function(req, res) {
-    var clientId = req.params.clientId;
-    console.log(tasks);
-    console.log(clientId);
+app.get('/get/:type', function(req, res) {
+    var type = req.params.type;
     try {
-        res.send(JSON.stringify(getTasks(clientId)));
+        res.send(JSON.stringify(getTasks(type))+"\n");
     } catch(e) {
         res.end();
     }
 });
 
-app.post('/upload/:taskId', function(req, res, next) {
+app.post('/upload/:type/:taskId', function(req, res, next) {
     var taskId = req.params.taskId;
+    var type = req.params.type;
 
     // Ref: https://github.com/visionmedia/express/blob/master/examples/multipart/app.js
     //      http://visionmedia.github.com/connect-form/
@@ -60,21 +68,29 @@ app.post('/upload/:taskId', function(req, res, next) {
         }
         
         for (var k in files) {
-            console.log('path: ' + files[k].path);
-            console.log('name: ' + files[k].name);
+            var f = files[k];
+            console.log('fileinfo === ');
+            console.log('path: ' + f.path);
+            console.log('name: ' + f.name);
             console.log('taskId: ' + taskId);
+
+            var task = tasks[taskId];
+            if (task) {
+                console.log(task);
+                var p = path.join(__dirname, task.path);
+                mkdirP(p, 488, function() {
+                    var newFilePath = p + type + path.extname(f.name);
+                    require('fs').rename(f.path, newFilePath);
+                });
+            }
         }
-        /*
-        console.log('uploaded %s to %s'
-                , files.image.filename
-                , files.image.path);
-        */
-        /*
-        var p = path.join(a, b);
-        mkdirP(path, 488);
-        */
-        res.send('ok');
+        
+        res.send("ok\n");
     });
+});
+
+app.get('/list/:path', function(req, res) {
+
 });
 
 
@@ -84,20 +100,25 @@ app.post('/upload/:taskId', function(req, res, next) {
 function addTask(url) {
     var taskId = uuid();
     var n = new Date();
+    var y = n.getFullYear();
+    var m = n.getMonth() + 1 + '';
+    var d = n.getDate();
+    m = m.length === 1 ? ('0'+m) : m;
+
     var task = {
         taskId: taskId,
         url: url,
         status: {},
-        date: '' + n.getFullYear() + (n.getMonth()+1) + n.getDate()
+        path: '../../imgs/'+y+'/'+m+'/'+d+'/'+taskId+'/'
     };
     tasks[taskId] = task;
 }
 
-function getTasks(clientId) {
+function getTasks(type) {
     var ret = [];
     for (var k in tasks) {
-        if (!tasks[k]['status'][clientId]) {
-            tasks[k]['status'][clientId] = true;
+        if (!tasks[k]['status'][type]) {
+            tasks[k]['status'][type] = true;
             ret.push([tasks[k]['url'], tasks[k]['taskId']]);
         }
     }
---
+++ src/server/tests/test.sh
@@ -0,0 +1,8 @@
+
+# curl http://test.taobao.com:8888/add?url=http%3A%2F%2Fwww.taobao.com%2F
+# curl http://test.taobao.com:8888/get/ie6
+
+curl -F "filename=@test.jpg" http://test.taobao.com:8888/upload/ie6/12345
+
+# curl -F "filename=@test.jpg" http://test.taobao.com:8888/upload/ie7/12345
+# curl -F "filename=@test.jpg" http://test.taobao.com:8888/upload/ie8/12345
