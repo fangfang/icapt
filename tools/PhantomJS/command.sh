e5d847d19be1891b6bb47ca4ddfd5803817db226
chencheng (yunqian) <sorrycc@gmail.com>
2011-06-21 10:03:18 +0800

    增加 ur

--- tools/PhantomJS/command.sh
+++ tools/PhantomJS/command.sh
@@ -1,1 +1,2 @@
-phantomjs phantom.js http://www.taobao.com taobao.png
+# phantomjs phantom.js http://www.taobao.com taobao.png
+phantomjs /Users/chencheng/Documents/Work/WorkSpace/icapt/tools/PhantomJS/phantom.js $1 $2
